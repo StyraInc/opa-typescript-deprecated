2b3930e17284de71e7087601b530859010bb9ee2
Stephan Renatus <stephan@styra.com>
2024-03-14 14:21:48 +0100

    sdk: add `authorizer` helper (#5)
    Also:
    * ci: add PR workflow
    
    Signed-off-by: Stephan Renatus <stephan@styra.com>

---
+++ src/helpers/authorizer.ts
@@ -0,0 +1,29 @@
+import { Opa } from "../sdk";
+import { Input } from "../models/components";
+import {
+  ExecutePolicyWithInputResponse,
+  ExecutePolicyResponse,
+} from "../models/operations";
+
+export interface ToInput {
+  toInput(): Input;
+}
+
+export function authorizer<In extends Input | ToInput, Res>(
+  sdk: Opa,
+  path: string,
+): (_?: In) => Promise<Res> {
+  return async function (input?: In): Promise<Res> {
+    let result: ExecutePolicyWithInputResponse | ExecutePolicyResponse;
+    if (input === undefined) {
+      result = await sdk.executePolicy({ path });
+    } else {
+      result = await sdk.executePolicyWithInput({
+        path,
+        requestBody: { input },
+      });
+    }
+    if (!result.successfulPolicyEvaluation) throw `no result in API response`;
+    return result.successfulPolicyEvaluation.result as Res;
+  };
+}
---
+++ tests/authorizer.test.ts
@@ -0,0 +1,131 @@
+import { describe, before, after, it } from "node:test";
+import assert from "node:assert";
+import { GenericContainer, StartedTestContainer, Wait } from "testcontainers";
+import { authorizer, ToInput } from "../src/helpers";
+import { Input } from "../src/models/components";
+import { Opa } from "../src/sdk";
+
+// Run these locally, with debug output from testcontainers, like this:
+// DEBUG='testcontainers*' node --require ts-node/register --test tests/**/*.ts
+
+describe("tests", async () => {
+  const policies = {
+    test: `package test
+import rego.v1
+
+p_bool if true
+
+p_bool_false if input == false
+
+has_type.type := type_name(input)
+
+compound_input.foo := "bar" if input == {"name": "alice", "list": [1, 2, true]}
+`,
+    slash: `package has["weird/package"].but
+import rego.v1
+
+it_is := true
+`,
+  };
+
+  let container: StartedTestContainer;
+  let serverURL: string;
+  before(async () => {
+    container = await new GenericContainer("openpolicyagent/opa:latest")
+      .withCommand([
+        "run",
+        "--server",
+        "--disable-telemetry",
+        "--log-level=debug",
+      ])
+      .withExposedPorts(8181)
+      .withWaitStrategy(Wait.forHttp("/health", 8181).forStatusCode(200))
+      .start();
+    serverURL = `http://${container.getHost()}:${container.getMappedPort(8181)}`;
+
+    for (const [id, body] of Object.entries(policies)) {
+      const response = await fetch(serverURL + "/v1/policies/" + id, {
+        method: "PUT",
+        headers: { "Content-Type": "text/plain" },
+        body,
+      });
+      assert.ok(response.ok);
+    }
+  });
+
+  it("can be called without types, without input", async () => {
+    const res = await authorizer(new Opa({ serverURL }), "test/p_bool")();
+    assert.strictEqual(res, true);
+  });
+
+  it("can be called with input==false", async () => {
+    const res = await authorizer(
+      new Opa({ serverURL }),
+      "test/p_bool_false",
+    )(false);
+    assert.strictEqual(res, true);
+  });
+
+  it("supports rules with slashes", async () => {
+    const res = await authorizer(
+      new Opa({ serverURL }),
+      "has/weird%2fpackage/but/it_is",
+    )();
+    assert.strictEqual(res, true);
+  });
+
+  it("supports input/result types", async () => {
+    interface myInput {
+      name: string;
+      list: any[];
+    }
+    interface myResult {
+      foo: string;
+    }
+    const inp: myInput = { name: "alice", list: [1, 2, true] };
+    const res = await authorizer<myInput, myResult>(
+      new Opa({ serverURL }),
+      "test/compound_input",
+    )(inp);
+    assert.deepStrictEqual(res, { foo: "bar" });
+  });
+
+  it("supports input of type bool", async () => {
+    interface typeResult {
+      type: string;
+    }
+    const inp = true;
+    const res = await authorizer<boolean, typeResult>(
+      new Opa({ serverURL }),
+      "test/has_type",
+    )(inp);
+    assert.deepStrictEqual(res, { type: "boolean" });
+  });
+
+  it("supports input class implementing ToInput", async () => {
+    class A implements ToInput {
+      private name: string;
+      private list: any[];
+
+      constructor(name: string, list: any[]) {
+        this.name = name;
+        this.list = list;
+      }
+
+      toInput(): Input {
+        return { name: this.name, list: this.list };
+      }
+    }
+    const inp = new A("alice", [1, 2, true]);
+    interface myResult {
+      foo: string;
+    }
+    const res = await authorizer<A, myResult>(
+      new Opa({ serverURL }),
+      "test/compound_input",
+    )(inp);
+    assert.deepStrictEqual(res, { foo: "bar" });
+  });
+
+  after(async () => await container.stop());
+});
