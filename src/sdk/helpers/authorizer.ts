227ea8b388910f6d0602727e9a5d37f4b329fcc2
Stephan Renatus <stephan@styra.com>
2024-03-14 20:28:05 +0100

    bug(authorizer): actually account for ToInput implementations
    Before, this had accidentally worked in the test case because
    - the object (instance of class A) had passed through the zod validation
    - and its JSON.stringify happened to be the right format
    
    Now, the test cases are better capturing the intent.
    
    Signed-off-by: Stephan Renatus <stephan@styra.com>

--- src/sdk/helpers/authorizer.ts
+++ src/sdk/helpers/authorizer.ts
@@ -9,18 +9,30 @@ export interface ToInput {
   toInput(): Input;
 }
 
+function implementsToInput(object: any): object is ToInput {
+  const u = object as ToInput;
+  return u.toInput !== undefined && typeof u.toInput == "function";
+}
+
 export function authorizer<In extends Input | ToInput, Res>(
   sdk: Opa,
   path: string,
 ): (_?: In) => Promise<Res> {
   return async function (input?: In): Promise<Res> {
     let result: ExecutePolicyWithInputResponse | ExecutePolicyResponse;
+
     if (input === undefined) {
       result = await sdk.executePolicy({ path });
     } else {
+      let inp: Input;
+      if (implementsToInput(input)) {
+        inp = input.toInput();
+      } else {
+        inp = input;
+      }
       result = await sdk.executePolicyWithInput({
         path,
-        requestBody: { input },
+        requestBody: { input: inp },
       });
     }
     if (!result.successfulPolicyEvaluation) throw `no result in API response`;
