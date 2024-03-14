1d025a26f5739ec8e89953d0ac4317ffbcd4438d
Stephan Renatus <stephan@styra.com>
2024-03-14 10:19:13 +0100

    request-path-hook: be more selective
    Signed-off-by: Stephan Renatus <stephan@styra.com>

--- src/hooks/request-path-hook.ts
+++ src/hooks/request-path-hook.ts
@@ -2,6 +2,10 @@ import { BeforeRequestContext, BeforeRequestHook } from "./types";
 
 export class RewriteRequestPathHook implements BeforeRequestHook {
   beforeRequest(_hookCtx: BeforeRequestContext, request: Request): Request {
-    return new Request(decodeURIComponent(request.url), request);
+    const url = new URL(request.url);
+    if (url.pathname.startsWith("/v1/data")) {
+      return new Request(decodeURIComponent(request.url), request);
+    }
+    return request;
   }
 }
