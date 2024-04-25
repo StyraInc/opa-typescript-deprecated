bf09be98b453c1734cfef6c81081653d74a41e36
Stephan Renatus <stephan@styra.com>
2024-04-25 10:43:20 +0200

    sdk: update request path fix to use new hook type
    The previous workaround here didn't work with using the
    SDK in Browsers: Firefox would silently drop the `body`
    from the request; Chrome would throw an exception about
    a missing `duplex` setting.
    
    Signed-off-by: Stephan Renatus <stephan@styra.com>

--- src/hooks/registration.ts
+++ src/hooks/registration.ts
@@ -5,5 +5,5 @@ export function initHooks(hooks: Hooks) {
   // Add hooks by calling hooks.register{ClientInit/BeforeRequest/AfterRequest/AfterError}Hook
   // with an instance of a hook that implements that specific Hook interface
   // Hooks are registered per SDK instance, and are valid for the lifetime of the SDK instance
-  hooks.registerBeforeRequestHook(new RewriteRequestPathHook());
+  hooks.registerBeforeCreateRequestHook(new RewriteRequestPathHook());
 }
--- src/hooks/request-path-hook.ts
+++ src/hooks/request-path-hook.ts
@@ -1,11 +1,16 @@
-import { BeforeRequestContext, BeforeRequestHook } from "./types";
+import { BeforeCreateRequestContext, BeforeCreateRequestHook } from "./types";
+import { RequestInput } from "../lib/http";
 
-export class RewriteRequestPathHook implements BeforeRequestHook {
-  beforeRequest(_hookCtx: BeforeRequestContext, request: Request): Request {
-    const url = new URL(request.url);
+export class RewriteRequestPathHook implements BeforeCreateRequestHook {
+  beforeCreateRequest(
+    _hookCtx: BeforeCreateRequestContext,
+    input: RequestInput,
+  ): RequestInput {
+    const url = new URL(input.url);
     if (url.pathname.startsWith("/v1/data")) {
-      return new Request(decodeURIComponent(request.url), request);
+      url.pathname = decodeURIComponent(url.pathname);
+      return { ...input, url };
     }
-    return request;
+    return input;
   }
 }
