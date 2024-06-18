963703c0e496f1b9bce268cc5af7c59cbc2c3e82
Stephan Renatus <stephan@styra.com>
2024-06-18 13:26:36 +0200

    sdk: add evaluateBatch()
    Signed-off-by: Stephan Renatus <stephan@styra.com>

--- src/hooks/request-path-hook.ts
+++ src/hooks/request-path-hook.ts
@@ -7,7 +7,10 @@ export class RewriteRequestPathHook implements BeforeCreateRequestHook {
     input: RequestInput,
   ): RequestInput {
     const url = new URL(input.url);
-    if (url.pathname.indexOf("/v1/data/") != -1) {
+    if (
+      url.pathname.indexOf("/v1/data/") != -1 ||
+      url.pathname.indexOf("/v1/batch/data/") != -1
+    ) {
       url.pathname = decodeURIComponent(url.pathname);
       return { ...input, url };
     }
